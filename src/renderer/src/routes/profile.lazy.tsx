98a7b7458fb7b3f425a760254cefaa3170955e8f
aalibrahimi <166450703+aalibrahimi@users.noreply.github.com>
2024-12-28 21:04:19 -0800

    Succesfully created 2 more routes,  profile and settings routes for the dropdown content

---
+++ src/renderer/src/routes/profile.lazy.tsx
@@ -0,0 +1,13 @@
+import { createLazyFileRoute } from '@tanstack/react-router'
+
+const profile = () => {
+    return (
+        <>
+            <h3>This is your profile</h3>
+        </>
+    )
+}
+
+export const Route = createLazyFileRoute('/profile')({
+    component: profile
+})
